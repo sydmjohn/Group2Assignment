d2854f581580aebafbf5b283e6195dcbff564e2f
Spencer Wood <Swood30@vols.utk.edu>
2022-03-11 16:04:54 -0500

    Feature 3 new
    Updates to Final Feature 3

--- Feature3.R
+++ Feature3.R
@@ -8,26 +8,37 @@ TOP10STOCKS <- tq_get(STOCK10, from = start, to = end)
 
 
 ui <- fluidPage( 
-  
+  theme = bslib::bs_theme(bootswatch = "darkly"),
   sidebarPanel(
+      
     radioButtons("radio", label = h3("Select Top Company"),
                  choices = 
                    c("BRK-A", "LDSVF", "NXGPF", "AMZN", "SEB", "NVR", "BKNg", "MKL", "MRF", "GOOGL")),
     dateInput("date", label = h3("Stock Date"), value = "2021-01-01")
   ),
+  titlePanel("Stock Close and Open"),
   
   hr(),
-  fluidRow(column(3, verbatimTextOutput("value")))
+  
+  mainPanel( "Closing",
+  fluidRow(column(3, verbatimTextOutput( "value")))
+  ),
+  mainPanel( "Opening",
+  fluidRow(column(3, verbatimTextOutput( "values"))))
 )
 
 server <- function(input, output, session) {
-  output$value <- renderPrint( 
-    ({ TOP10STOCKS$close[which(TOP10STOCKS$date == input$date &
-                                 TOP10STOCKS$symbol == input$radio)]
+  output$value <- renderPrint({ 
+      TOP10STOCKS$close[which(TOP10STOCKS$date == input$date &
+                                 TOP10STOCKS$symbol == input$radio)]})
       
-    })
-    
-  )
+      
+  output$values <- renderPrint( 
+        ({  TOP10STOCKS$open[which(TOP10STOCKS$date == input$date &
+                                      TOP10STOCKS$symbol == input$radio)]})
+      )
+      
+  
 }
 
 shinyApp(ui = ui, server = server)
